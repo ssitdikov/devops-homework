0b3acac42feaf8c5596b869ca47502a2453f8a45
Salavat Sitdikov <sitsalavat@gmail.com>
2021-02-17 19:56:14 +0300

    Fix issue #1; add parameters; refactoring

--- 1/README.md
+++ 1/README.md
@@ -1,10 +1,10 @@
-# What is about connection
+# What is about our connections?
 
 Magic script for getting information by connections.
 
 You can use script:
 - step by step
-- by parametrization (_coming soon_)
+- by parametrization
 
 For running step by step you must run script without parameters for filtering and fetching num of rows:
 ```shell
@@ -17,3 +17,8 @@ It calls navigation menu for setting PID or/and name of process, result of rows 
 3) Set up max result rows
 0) Run
 ```
+Another way is using parameters. Some examples:
+```shell
+./whocn.sh -p 2010 -n "another" -r 10  
+```
+It returns last `10` records of connections with PID `2010` processes and name - `another`
--- 1/whocn.sh
+++ 1/whocn.sh
@@ -9,11 +9,16 @@ params=""
 
 digits='^[0-9]+$'
 
+function get_info() {
+  run="sudo netstat -tunapl $params | cut -d: -f1 | sort | uniq -c | sort | tail -n$rows | grep -oP '(\d+\.){3}\d+' | while read IP ; do whois \$IP | awk -F':' '/^Organization/ {print \$2}' ; done"
+  eval $run
+}
+
 function help() {
-  echo "Parser of netstat"
+  echo "Parser of connections"
   echo
   echo "Usage: $SCRIPT options"
-  echo "-p , pid of process"
+  echo "-p , PID of process"
   echo "-n , name of process"
   echo "-r , num of result rows"
   echo
@@ -21,54 +26,81 @@ function help() {
 
 function selector() {
   echo "Please, select operation:"
-  echo "1) Set up PID or name of process"
+  echo "1) Set up PID of process"
   echo "2) Set up name of process"
   echo "3) Set up max result rows"
-  echo "4) Wanna see another connection states? (not available now)"
+  #  echo "4) Wanna see another connection states? (not available now)"
   echo "0) Run"
-  echo -n "Operation: "
 
-  read -r operation
-
+  read -p "Operation: " operation
   case $operation in
   "1" | "1)")
-    echo -n "Enter PID of process: "
-    read -r pid
-    if ! [[ "$pid" =~ $digits ]]; then
-      echo "Oh no! Incorrect port number!"
-    else
-      params="$params | grep \":$pid \""
-    fi
-
+    read -p "Enter PID of process: " pid
+    set_pid "$pid"
     selector
     ;;
   "2" | "2)")
-    echo -n "Enter name of process: "
-    read -r name
-    if [ -z "$name" ]; then
-      params="$params | awk '/firefox/ {print \$5}'"
-    else
-      params="$params | awk '/$name/ {print \$5}'"
-    fi
+    read -p "Enter name of process: " name
+    set_process_name "$name"
+
     selector
     ;;
   "3" | "3)")
-    echo -n "Enter max result rows: "
-    read -r rows
-    if ! [[ "$rows" =~ $digits ]]; then
-      echo "It is not integer value of rows!"
-      rows=5
-    fi
+    read -p "Enter max result rows: " rows_num
+    set_result_rows "$rows_num"
+    selector
+    ;;
+  "4" | "4)")
+    echo "Coming soon..."
     selector
     ;;
   "0" | "0)")
-    run="sudo netstat -tunapl $params | cut -d: -f1 | sort | uniq -c | sort | tail -n$rows | grep -oP '(\d+\.){3}\d+' | while read IP ; do whois \$IP | awk -F':' '/^Organization/ {print \$2}' ; done"
-    eval $run
+    get_info
     ;;
   esac
 
 }
 
+function set_pid() {
+  if ! [[ "$1" =~ $digits ]]; then
+    echo "Oh no! Incorrect PID!"
+  else
+    params="$params | grep \" $1\/\""
+  fi
+}
+
+function set_process_name() {
+  if [ -z "$1" ]; then
+    params="$params | awk '/firefox/ {print \$5}'"
+  else
+    params="$params | awk '/$1/ {print \$5}'"
+  fi
+}
+
+function set_result_rows() {
+  rows="$1"
+}
+
 if [ $# = 0 ]; then
   selector
+else
+  while [ -n "$1" ]; do
+    case "$1" in
+    -p)
+      set_pid $2
+      shift
+      ;;
+    -n)
+      set_process_name $2
+      shift
+      ;;
+    -r)
+      set_result_rows $2
+      shift
+      ;;
+    *) echo "$1 is not an option" ;;
+    esac
+    shift
+  done
+  get_info
 fi
