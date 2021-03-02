a033654d8e69b6acc6afa48012b3063b1e5fcd85
Salavat Sitdikov <sitsalavat@gmail.com>
2021-03-03 01:35:02 +0300

    Fix issue with not found response

--- 3/gethubinfo.sh
+++ 3/gethubinfo.sh
@@ -4,6 +4,10 @@ clear
 
 function get_open_pr_info() {
   PR_REQUESTS=$(curl -sb -H "Accept: application/json" "$1/pulls")
+  if [[ $(echo "$PR_REQUESTS" | jq '.[]') =~ "Not Found" ]]; then
+    echo "Repository is not existed"
+    exit
+  fi
   OPEN_PR_NUM=$(echo "$PR_REQUESTS" | jq '. | length')
   if [ "$OPEN_PR_NUM" -eq "0" ]; then
     echo "There is no open pull requests"
@@ -13,15 +17,17 @@ function get_open_pr_info() {
     else
       echo "There are $OPEN_PR_NUM open pull requests"
     fi
-  fi
-  echo "Is anybody productive contributors here?"
-  echo "$PR_REQUESTS" | jq -r '.[].user.login' | uniq -c | sort -r | awk '$1 > 1 {print $2" has "$1" open PR"}'
 
-  echo "Author and name of PRs"
-  JSON_RESULT=$(echo "$PR_REQUESTS" | jq -r 'group_by(.user.login)[] | {(.[0].user.login): [.[] | .title] | join("; ")}')
-  echo $JSON_RESULT | jq -r 'keys[] as $k | "- \($k): \(.[$k])"'
+    echo "Is anybody productive contributors here?"
+    echo "$PR_REQUESTS" | jq -r '.[].user.login' | uniq -c | sort -r | awk '$1 > 1 {print $2" has "$1" open PR"}'
+
+    echo "Author and name of PRs"
+    JSON_RESULT=$(echo "$PR_REQUESTS" | jq -r 'group_by(.user.login)[] | {(.[0].user.login): [.[] | .title] | join("; ")}')
+    echo "$JSON_RESULT" | jq -r 'keys[] as $k | "- \($k): \(.[$k])"'
+
+  fi
 }
 
 BASE_API_URL=$(echo $1 | awk -F/ '{api_url="https://api.github.com/repos/"$4"/"$5; print api_url}')
 
-get_open_pr_info $BASE_API_URL
+get_open_pr_info "$BASE_API_URL"
